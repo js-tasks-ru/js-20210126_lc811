214bd3059dc173ac4b66f3781bb55455e489f4c4
Andrey Shendrik <lc811@yandex.ru>
2021-02-11 17:39:39 +0300

    05 module Notification task solved

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,58 @@
 export default class NotificationMessage {
+  static onShow = null;
 
+  constructor(message = 'Hello World!', {duration = 1000, type = 'success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.createElement();
+  }
+
+  get template() {
+    return `
+    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+            <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+            ${this.message}
+            </div>
+        </div>
+    </div>`;
+  }
+
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  remove() {
+    if (!NotificationMessage.onShow) {
+      return;
+    } else {
+      clearTimeout(NotificationMessage.onShow.timerId);
+      NotificationMessage.onShow.instance.element.remove();
+      NotificationMessage.onShow = null;
+    }
+  }
+    
+  setOnShow(timerId) {
+    NotificationMessage.onShow = {
+      instance: this,
+      timerId: timerId
+    };
+  }
+
+  show(element = document.body) {
+    this.remove();
+    element.append(this.element);
+    const timerId = setTimeout(this.remove, this.duration);
+    this.setOnShow(timerId);
+  }
+
+  destroy() {
+    this.element.remove();
+    this.onShow = null;
+  }
 }
